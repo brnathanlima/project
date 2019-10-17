c793457d56d3dd44f7e67559222784cd31995b49
Nathanael Lima <nathanaellimacpc@gmail.com>
2019-10-16 23:52:36 -0300

    Created rest routes and entire edit of projects functionality

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -31,4 +31,33 @@ class ProjectsController extends Controller
 
         return redirect('/projects');
     }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+        $project = Project::find($id);
+
+        return view('projects.edit', compact('project'));
+    }
+
+    public function update($id)
+    {
+        $project = Project::find($id);
+
+        $project->title = request('title');
+        $project->description = request('description');
+
+        $project->save();
+
+        return redirect('/projects');
+    }
+
+    public function destroy($id)
+    {
+
+    }
 }
