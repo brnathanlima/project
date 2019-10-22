e9937141b853c488a3cef7a185c7278a9160d93e
Nathanael Lima <nathanaellimacpc@gmail.com>
2019-10-21 23:08:16 -0300

    Adjustments on project creation form

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -5,18 +5,28 @@
 @section('content')
     <h1 class="title">Edit Project</h1>
 
+    @if ($errors->any())
+        <div class="notification is-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form method="POST" action="/projects">
         @csrf
         <div class="field">
             <label for="title" class="label">Title</label>
             <div class="control">
-                <input type="text" class="input" name="title" placeholder="Title">
+                <input type="text" class="input {{ $errors->has('title') ? 'is-danger' : '' }}" name="title" placeholder="Title" value="{{ old('title') }}">
             </div>
         </div>
         <div class="field">
             <label for="description" class="label">description</label>
             <div class="control">
-                <textarea name="description" id="description" cols="30" rows="10" class="textarea"></textarea>
+                <textarea name="description" id="description" cols="30" rows="10" class="textarea {{ $errors->has('description') ? 'is-danger' : ''}}">{{ old('description') }}</textarea>
             </div>
         </div>
         <div class="field">
